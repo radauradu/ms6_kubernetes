c524a26b02ce0d34095a3e2a46573d261e06ed24
Radu Radau <radu.radau@RO-C69T7NHJGC.local>
2022-11-04 15:13:54 +0200

    8th commit

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  shared_credentials_files = ["/Users/radu.radau/.aws/credentials"]
+  #shared_credentials_files = ["/Users/radu.radau/.aws/credentials"]
   region = "eu-central-1"
 }
